7f0e93f6cb10b08a157f70f4bcd9a45dde2124db
Salah Hamsate <shamsate@e1r6p2.1337.ma>
2024-12-19 16:59:32 +0100

    add some geters & setters for Client to make authentification waork as expected

--- include/Client.hpp
+++ include/Client.hpp
@@ -55,7 +55,7 @@ class Client{
         void setnflg(bool flg);
         void setuflg(bool flg);
 /* *************************************************** */
-    bool		isAuthed();
+        bool		isAuthed();
 	bool		getpflag();
 	bool		getnflag();
 	bool		getuflag();
