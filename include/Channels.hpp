852462cf64abcdb152f6529b5907784df3219b9c
Salah Hamsate <shamsate@e1r10p2.1337.ma>
2024-12-24 17:24:53 +0100

    create Channels and some peer fcnt

--- include/Channels.hpp
+++ include/Channels.hpp
@@ -10,3 +10,63 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CHANNELS_HPP
+#define CHANNELS_HPP
+
+#include "Server.hpp"
+#include "Client.hpp"
+
+class Channels{
+    private :
+        bool            _inv;
+        bool            _flgPass;
+        bool            _topc;
+        bool            _userLimit;
+        size_t          _limit;
+        size_t          _userSize;
+        std::string     _name;
+        std::string     _topicSetter;
+        std::string     _topicTime;
+        std::string     _pass;
+        std::string     _topic;
+        std::string     _nmChDisplay;
+        std::map<std::pair<bool, int>, Client>_users;
+        std::vector<int>_invited;
+    public :
+        Channels(std::string    nmCh);
+        void pushToMap(bool priv, Client &cli);
+        //:::::::::::::::::setter::::::::::::::::::
+        void setPass(std::string pass);
+        void setFlgpass(bool flgpass);
+        void setSizeUser(size_t sizeuser);
+        void setNmDisplay(std::string name);
+        void setInv(bool inv);
+        void setTopic(std::string topic) ;
+        void  setTopicAttr(std::string topic, bool topc, std::string seter, std::string time);
+       //:::::::::::::::::Getter::::::::::::::::::
+        bool            getInv();
+        bool            getSiUserLimit();
+        std::string     getPass();
+        bool            getFlgpass();
+        size_t          getSizeuser();
+        std::string     getNmCh();
+        size_t          getLimit();
+        std::string     getNmChDispaly();
+        std::map<std::pair<bool,int>, Client >& getMapUser();
+        std::string     getTopic();
+        bool            getTopicBool();
+        std::string     getTopicSeter();
+        std::string     getTopicTime();
+        bool            checkIfOperat(std::string nickname);
+        bool            nickExist(std::string user);
+        //
+        bool	        isInvited(int sockfd);
+        void            deletCli(std::string nick);
+
+
+}
+
+
+
+
+#endif
